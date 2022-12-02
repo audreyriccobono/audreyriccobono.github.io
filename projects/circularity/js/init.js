8983134ec886ad76366e868082d7c9f34c84aaa0
audreyriccobono <audrey.b.riccobono@gmail.com>
2022-12-02 15:59:35 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -32,14 +32,18 @@ var init = function (window) {
         circles.push(circle)
         }
         // TODO 3 / 7 : Call the drawCircle() function 
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        for (var drawCircle = 0; drawCircle < 100; drawCircle++) {
-            
+        
+        for (var x = 0; x < 100; x++){
+            drawCircle();
         }
+
+
+
+
+        
+
+
+        
 
 
         ////////////////////////////////////////////////////////////
@@ -53,22 +57,20 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition(circles[0]);
-            physikz.updatePosition(circles[1]);
-            physikz.updatePosition(circles[2]);
-            physikz.updatePosition(circles[3]);
-            physikz.updatePosition(circles[4]);
+          
         
 
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition( circles[0]);
-            game.checkCirclePosition( circles[1]);
-            game.checkCirclePosition( circles[2]);
-            game.checkCirclePosition( circles[3]);
-            game.checkCirclePosition( circles[4]);
+           
             
 
             // TODO 9 : Iterate over the array
+            for (var i = 0; i < circles.length; i++){
+                var circle = circles[i];
+                game.checkCirclePosition(circle)
+                physikz.updatePosition(circle)
+                
+            }
         
          
             
